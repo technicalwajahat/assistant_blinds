ee9f9ac096eb8b9e05f7a1b208cd1aa48fbaadd2
technicalwajahat <wajahat.awan.924@gmail.com>
2024-03-07 01:46:43 +0500

    updates

--- lib/views/text_to_speech.dart
+++ lib/views/text_to_speech.dart
@@ -1,4 +1,5 @@
 import 'package:assistant_blinds/widgets/appBar.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class TextToSpeechScreen extends StatefulWidget {
@@ -11,8 +12,27 @@ class TextToSpeechScreen extends StatefulWidget {
 class _TextToSpeechScreenState extends State<TextToSpeechScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBarWidget(text: "Text to Speech"),
+    return Scaffold(
+      appBar: const AppBarWidget(text: "Text to Speech"),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Upload Document",
+        onPressed: () {
+          _pickFiles();
+        },
+        child: const Icon(Icons.upload_file_rounded),
+      ),
     );
+  }
+
+  Future<void> _pickFiles() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['ppt', 'pdf', 'doc'],
+    );
+    if (result != null) {
+      print(result.files.first.path);
+    } else {
+      print("No file picked.");
+    }
   }
 }
