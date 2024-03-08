f3b8822b443a69b0c284f5b03783abe0a6578b77
technicalwajahat <wajahat.awan.924@gmail.com>
2024-03-09 02:12:17 +0500

    updates

--- lib/controller/text_to_speech_controller.dart
+++ lib/controller/text_to_speech_controller.dart
@@ -1,3 +1,34 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:get/get.dart';
+import 'package:open_file/open_file.dart';
 
-class TextSpeechController extends GetxController {}
+class TextSpeechController extends GetxController {
+  PlatformFile? pickedFilePath;
+
+  Future<void> pickFiles() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['ppt', 'pdf', 'doc', 'docx'],
+      allowMultiple: false,
+    );
+    if (result != null) {
+      final file = result.files.single;
+      pickedFilePath = file;
+      update();
+    }
+  }
+
+  String getFileSizeText(int size) {
+    if (size <= 1024) {
+      return '$size bytes';
+    } else if (size <= 1048576) {
+      return '${(size / 1024).toStringAsFixed(2)} KB';
+    } else {
+      return '${(size / 1048576).toStringAsFixed(2)} MB';
+    }
+  }
+
+  Future<void> openFile(String? filePath) async {
+    await OpenFile.open(filePath);
+  }
+}
--- lib/views/text_to_speech.dart
+++ lib/views/text_to_speech.dart
@@ -1,10 +1,9 @@
 import 'package:assistant_blinds/widgets/appBar.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:path/path.dart' as path;
+
+import '../controller/text_to_speech_controller.dart';
 
 class TextToSpeechScreen extends StatefulWidget {
   const TextToSpeechScreen({super.key});
@@ -14,7 +13,7 @@ class TextToSpeechScreen extends StatefulWidget {
 }
 
 class _TextToSpeechScreenState extends State<TextToSpeechScreen> {
-  PlatformFile? pickedFilePath;
+  final textToSpeechController = Get.put(TextSpeechController());
 
   @override
   Widget build(BuildContext context) {
@@ -22,130 +21,96 @@ class _TextToSpeechScreenState extends State<TextToSpeechScreen> {
       appBar: const AppBarWidget(text: "Text to Speech"),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-        child: Column(
-          children: [
-            InkWell(
-              onTap: () {
-                _pickFiles();
-              },
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Card(
-                  elevation: 5,
-                  margin: EdgeInsets.zero,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(Icons.upload_file_rounded, size: 34),
-                        SizedBox(height: Get.height * 0.01),
-                        const AutoSizeText(
-                          "Upload Document",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
+        child:
+            GetBuilder<TextSpeechController>(builder: (textToSpeechController) {
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  textToSpeechController.pickFiles();
+                },
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Card(
+                    elevation: 5,
+                    margin: EdgeInsets.zero,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(Icons.upload_file_rounded, size: 34),
+                          SizedBox(height: Get.height * 0.01),
+                          const AutoSizeText(
+                            "Upload Document",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: Get.height * 0.04),
-            pickedFilePath != null
-                ? Column(
-                    children: [
-                      // AutoSizeText(
-                      //   textAlign: TextAlign.center,
-                      //   '$pickedFilePath',
-                      //   style: const TextStyle(
-                      //       fontSize: 14, fontWeight: FontWeight.w600),
-                      // ),
-                      ListTile(
-                        title: AutoSizeText(
-                          pickedFilePath!.name,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 14),
-                        ),
-                        // File name
-                        subtitle: AutoSizeText(
-                          path.basename(
-                            pickedFilePath!.path.toString(),
+              SizedBox(height: Get.height * 0.05),
+              textToSpeechController.pickedFilePath != null
+                  ? Column(
+                      children: [
+                        ListTile(
+                          title: AutoSizeText(
+                            textToSpeechController.pickedFilePath!.name,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 14),
+                          ),
+                          subtitle: AutoSizeText(
+                            "File Size: ${textToSpeechController.getFileSizeText(textToSpeechController.pickedFilePath!.size)}",
+                          ),
+                          leading: const Icon(
+                            Icons.file_copy_rounded,
+                            size: 28,
+                          ),
+                          dense: true,
+                          trailing: IconButton(
+                            icon: const Icon(Icons.open_in_new_rounded),
+                            onPressed: () => textToSpeechController.openFile(
+                                textToSpeechController.pickedFilePath!.path),
                           ),
                         ),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.open_in_new),
-                          onPressed: () =>
-                              _openFile(pickedFilePath!.path.toString()),
+                        SizedBox(height: Get.height * 0.025),
+                        FilledButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size.fromHeight(50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const AutoSizeText(
+                            "Convert Text to Speech",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  : const Center(
+                      child: AutoSizeText(
+                        "Please Select Document",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(height: Get.height * 0.02),
-                      FilledButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size.fromHeight(50),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: const AutoSizeText(
-                          "Convert Text to Speech",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                : const Center(
-                    child: AutoSizeText(
-                      "Please Select Document",
-                      style:
-                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                    ),
-                  )
-          ],
-        ),
+                    )
+            ],
+          );
+        }),
       ),
     );
-  }
-
-  Future<void> _pickFiles() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ['ppt', 'pdf', 'doc', 'docx'],
-      allowMultiple: false,
-    );
-    if (result != null) {
-      final file = result.files.single;
-      setState(() {
-        pickedFilePath = file;
-      });
-    } else {
-      print("No file picked.");
-    }
-  }
-
-  String _getFileSizeText(int size) {
-    if (size <= 1024) {
-      return '$size bytes';
-    } else if (size <= 1048576) {
-      return '${(size / 1024).toStringAsFixed(2)} KB';
-    } else {
-      return '${(size / 1048576).toStringAsFixed(2)} MB';
-    }
-  }
-
-  Future<void> _openFile(String filePath) async {
-    final url = Uri.file(filePath);
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
-      print('Could not launch $url');
-    }
   }
 }
