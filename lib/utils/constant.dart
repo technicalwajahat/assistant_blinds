979ae44e0dcd525485701500bead15dc89a95635
technicalwajahat <wajahat.awan.924@gmail.com>
2024-05-10 21:32:13 +0500

    updates

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,1 +1,3 @@
-const openAIAPIKey = 'sk-proj-j17g6T6vFsJZoeAljNK4T3BlbkFJkqirfE4x6njVSRmC00qt';
+// const openAIAPIKey = 'sk-proj-j17g6T6vFsJZoeAljNK4T3BlbkFJkqirfE4x6njVSRmC00qt';
+const openAIAPIKey = 'sk-proj-kLz3h2JEbcsN9bpXBwLdT3BlbkFJDCINQQ1DJ8EOr42ySxVa';
+const GEMINI_API_KEY = 'AIzaSyDBJSUdoLsmT-cTitBQMiWFa2D8H0hY_Ks';
