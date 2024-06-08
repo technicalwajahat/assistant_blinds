23f4353070ad26202fad7cf003264ec988c92a55
technicalwajahat <wajahat.awan.924@gmail.com>
2024-06-08 11:30:07 +0500

    final updates

--- lib/views/TextToSpeech/saved_audio.dart
+++ lib/views/TextToSpeech/saved_audio.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:assistant_blinds/widgets/appBar.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
 
 class SavedAudio extends StatefulWidget {
   const SavedAudio({super.key});
@@ -12,53 +14,186 @@ class SavedAudio extends StatefulWidget {
 }
 
 class _SavedAudioState extends State<SavedAudio> {
-  late Future<List<FileSystemEntity>> _audioFiles;
+  late AudioPlayer _audioPlayer;
+  late File _selectedAudioFile;
 
   @override
   void initState() {
     super.initState();
-    getAudioFiles();
-    _audioFiles = getAudioFiles();
+    _audioPlayer = AudioPlayer();
+    _init();
+    _audioPlayer.positionStream;
+    _audioPlayer.bufferedPositionStream;
+    _audioPlayer.durationStream;
   }
 
-  Future<List<FileSystemEntity>> getAudioFiles() async {
-    Directory? musicDirectory = await getApplicationDocumentsDirectory();
-    String musicPath = musicDirectory.path;
-    final files = Directory(musicPath).listSync(recursive: true);
-    return files.where((file) => file.path.endsWith('.mp3')).toList();
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
   }
 
-  void _playAudio(String filePath) async {
-    // final player = AudioPlayer();
-    // await player.setAudioSource(AudioSource.uri(Uri.parse(filePath)));
-    // player.play();
+  Future<void> _init() async {
+    await _audioPlayer.setLoopMode(LoopMode.all);
+    await listMp3Files().then((files) {
+      if (files.isNotEmpty) {
+        setState(() {
+          _selectedAudioFile = files.first;
+          _audioPlayer.setFilePath(_selectedAudioFile.path);
+        });
+      }
+    });
+  }
+
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+        _audioPlayer.positionStream,
+        _audioPlayer.bufferedPositionStream,
+        _audioPlayer.durationStream,
+        (position, bufferedPosition, duration) => PositionData(
+          position,
+          bufferedPosition,
+          duration ?? Duration.zero,
+        ),
+      );
+
+  Future<List<File>> listMp3Files() async {
+    try {
+      const result = "/sdcard/Music";
+      final directory = Directory(result);
+      final mp3Files = await directory
+          .list()
+          .where((entity) => entity.path.endsWith('.mp3'))
+          .map((entity) => File(entity.path))
+          .toList();
+
+      print(mp3Files);
+      return mp3Files;
+    } catch (e) {
+      print("Error: $e");
+      return [];
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AppBarWidget(text: "Saved Audios"),
-      body: FutureBuilder<List<FileSystemEntity>>(
-        future: _audioFiles,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final files = snapshot.data;
-            return ListView.builder(
-              itemCount: files!.length,
-              itemBuilder: (context, index) {
-                final file = files[index];
-                return ListTile(
-                  title: Text(file.path.split('/').last),
-                  onTap: () => _playAudio(file.path),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return const Center(child: Text('Error fetching audio files'));
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
+      appBar: const AppBarWidget(text: "Saved Audio"),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(2, 4),
+                      blurRadius: 4,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(40),
+                  child: Image.asset(
+                    "assets/audi_icon.jpg",
+                    height: 350,
+                    width: 350,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              StreamBuilder<PositionData>(
+                stream: _positionDataStream,
+                builder: (context, snapshot) {
+                  final positionData = snapshot.data;
+                  return ProgressBar(
+                    barHeight: 8,
+                    baseBarColor: Colors.grey,
+                    bufferedBarColor: Colors.grey,
+                    progressBarColor: Colors.green,
+                    thumbColor: Colors.green,
+                    timeLabelTextStyle: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                    ),
+                    progress: positionData?.position ?? Duration.zero,
+                    total: positionData?.duration ?? Duration.zero,
+                    buffered: positionData?.bufferedPosition ?? Duration.zero,
+                    onSeek: _audioPlayer.seek,
+                  );
+                },
+              ),
+              Controls(audioPlayer: _audioPlayer),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class PositionData {
+  const PositionData(this.position, this.bufferedPosition, this.duration);
+
+  final Duration position;
+  final Duration bufferedPosition;
+  final Duration duration;
+}
+
+class Controls extends StatelessWidget {
+  const Controls({
+    super.key,
+    required this.audioPlayer,
+  });
+
+  final AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: audioPlayer.seekToPrevious,
+          icon: const Icon(Icons.skip_previous_rounded),
+          iconSize: 60,
+        ),
+        StreamBuilder<PlayerState>(
+          stream: audioPlayer.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final processingState = playerState?.processingState;
+            final playing = playerState?.playing;
+
+            if (!(playing ?? false)) {
+              return IconButton(
+                  onPressed: audioPlayer.play,
+                  iconSize: 80,
+                  icon: const Icon(Icons.play_arrow_rounded));
+            } else if (processingState != ProcessingState.completed) {
+              return IconButton(
+                onPressed: audioPlayer.pause,
+                icon: const Icon(Icons.pause_rounded),
+                iconSize: 80,
+              );
+            }
+            return const Icon(
+              Icons.play_arrow_rounded,
+              size: 80,
+            );
+          },
+        ),
+        IconButton(
+          onPressed: audioPlayer.seekToNext,
+          icon: const Icon(Icons.skip_next_rounded),
+          iconSize: 60,
+        ),
+      ],
     );
   }
 }
--- lib/views/TextToSpeech/view_speech.dart
+++ lib/views/TextToSpeech/view_speech.dart
@@ -3,6 +3,8 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ViewSpeech extends StatefulWidget {
   const ViewSpeech({super.key});
@@ -12,19 +14,55 @@ class ViewSpeech extends StatefulWidget {
 }
 
 class _ViewSpeechState extends State<ViewSpeech> {
+  final FlutterTts flutterTts = FlutterTts();
   var arguments = Get.arguments;
-  late FlutterTts flutterTts;
+  String? appDocumentsDirectory;
 
   @override
   void initState() {
     super.initState();
-    flutterTts = FlutterTts();
-    convertTextToSpeech("Wajahat Awan King!");
+    _checkPermissions();
+  }
+
+  Future<void> _checkPermissions() async {
+    final storageStatus = await Permission.storage.request();
+    if (storageStatus.isGranted) {
+      appDocumentsDirectory = getApplicationDocumentsDirectory().toString();
+      convertTextToSpeech(arguments);
+    } else {
+      _handlePermissionDenied();
+    }
+  }
+
+  void _handlePermissionDenied() {
+    Get.dialog(
+      AlertDialog(
+        title: const Text('Storage Permission Required'),
+        content: const Text(
+            'This app needs storage permission to save spoken text.'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Permission.storage.request(),
+            child: const Text('Request Permission'),
+          ),
+        ],
+      ),
+    );
   }
 
   Future<void> convertTextToSpeech(String text) async {
     await flutterTts.speak(text);
-    await flutterTts.synthesizeToFile(text, "blind_tts.mp3");
+    if (appDocumentsDirectory != null) {
+      try {
+        await flutterTts.synthesizeToFile(text, 'blind_tts.mp3');
+      } catch (error) {
+        print('Error saving file: $error');
+      }
+    }
   }
 
   @override
